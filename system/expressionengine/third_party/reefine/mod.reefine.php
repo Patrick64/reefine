f2cfc4b4d79a48a8f34881a885dbfd93a3feb94f
Patrick Woodcock <patrick@ralphmedia.co.uk>
2014-02-12 11:03:11 +0000

    Added on sale parameter

--- system/expressionengine/third_party/reefine/mod.reefine.php
+++ system/expressionengine/third_party/reefine/mod.reefine.php
@@ -1353,21 +1353,42 @@ class Reefine_field_store extends Reefine_field {
 		$this->field_name = $field_name;
 		$this->child_column=$child_column;
 		$this->table_alias = 'store_products_' . preg_replace('/[^A-Z0-9]/i','_',$ee_field_name);
-		
+		$this->sales_alias = 'store_sales_' . preg_replace('/[^A-Z0-9]/i','_',$ee_field_name);
+		$this->sales_cat_alias = 'store_sales_cat_' . preg_replace('/[^A-Z0-9]/i','_',$ee_field_name);
 	}
 	
 	
 	function get_value_column() {
-		return "{$this->table_alias}.{$this->child_column}";
+		if ($this->child_column=='on_sale') 
+			return "(CASE WHEN {$this->sales_alias}.enabled = 1 THEN '1' ELSE '' END)";			
+		else
+			return "{$this->table_alias}.{$this->child_column}";
 	}
 	
 	function get_title_column() {
-		return $this->get_value_column();
+		if ($this->child_column=='on_sale')
+			return "(CASE WHEN {$this->sales_alias}.enabled = 1 THEN 'On sale' ELSE '' END)";
+		else
+			return $this->get_value_column();
+		
 	}
 	
 	function get_join_sql() {
-		return "LEFT OUTER JOIN {$this->reefine->dbprefix}store_products {$this->table_alias} " .
-			"ON {$this->table_alias}.entry_id = {$this->channel_data_alias}.entry_id ";
+		$joins = array("LEFT OUTER JOIN {$this->reefine->dbprefix}store_products {$this->table_alias} " .
+			"ON {$this->table_alias}.entry_id = {$this->channel_data_alias}.entry_id ");
+		// if we want the on_sale parameter then we have to check the start/end date of the sale
+		// then check if the current entry is listed in the entry_ids column or if the entry's category id
+		// is in the category_ids column, both columns are pipe delimited numbers
+		if ($this->child_column=='on_sale')
+			$joins[] = " LEFT OUTER JOIN {$this->reefine->dbprefix}category_posts {$this->sales_cat_alias}
+			ON {$this->sales_cat_alias}.entry_id = {$this->channel_data_alias}.entry_id
+			LEFT OUTER JOIN {$this->reefine->dbprefix}store_sales {$this->sales_alias} 
+			ON {$this->sales_alias}.start_date<=UNIX_TIMESTAMP() AND  {$this->sales_alias}.end_date>=UNIX_TIMESTAMP() 
+			AND (({$this->sales_alias}.entry_ids<>'' 
+				AND LOCATE(concat('|',{$this->channel_data_alias}.entry_id,'|'),concat('|',{$this->sales_alias}.entry_ids,'|'))>0) 
+			OR ({$this->sales_alias}.category_ids <> ''
+				AND LOCATE(concat('|',{$this->sales_cat_alias}.cat_id,'|'),concat('|',{$this->sales_alias}.category_ids,'|'))>0)) \n";
+		return $joins;
 	}
 	
 }
