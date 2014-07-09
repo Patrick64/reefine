265073be15ea800d8d99014eceae87c437797dff
Patrick Woodcock <patrick@ralphmedia.co.uk>
2014-07-09 23:28:50 +0100

    added Reefine_field_publisher_playa

--- system/expressionengine/third_party/reefine/mod.reefine.php
+++ system/expressionengine/third_party/reefine/mod.reefine.php
@@ -1314,27 +1314,37 @@ class Reefine {
 	 * @return Reefine_field
 	 */
 	function get_field_obj($field_name) {
+		// field base is the default class name for the field
+		$field_base = 'Reefine_field';
+		// Publisher module detected so check the publisher fields instead
+		if (isset($this->EE->publisher_model)) {
+			$field_base = 'Reefine_field_publisher';
+		}
 		// if field name conmtains a colon
 		if (strpos($field_name,':')!==false) {
 			list($ee_field_name,$child_field) = explode(':', $field_name);
 			$field_type = $this->_custom_fields[$this->site][$ee_field_name]['field_type'];
-			$field_class = 'Reefine_field_' . $field_type;
+			$field_class = $field_base . '_' . $field_type;
 			if (class_exists($field_class)) {
 				return new $field_class($this,$field_name, $ee_field_name,$child_field);
 			} else {
 				throw new Exception('Reefine error: Fieldtype ' . $field_type . ' not supported.');
 			}
-		} else if (isset($this->EE->publisher_model)) {
-			// Publisher module detected so check the publisher fields instead
-			return new Reefine_field_publisher($this,$field_name, $field_name);
 		} else {
+			// field doesnt have a colon - lets look at the field type anyway
 			$field_type = $this->_custom_fields[$this->site][$field_name]['field_type'];
-			if ($field_type=='relationship')
-				return new Reefine_field_relationship($this, $field_name, $field_name, '');
-			if ($field_type=='playa')
-				return new Reefine_field_playa($this, $field_name, $field_name, '');
-			else
-				return new Reefine_field($this, $field_name);
+			// if the filter is a relationship/playa field and no subfield is specified then we show the title in the filter
+			if ($field_type=='relationship' || $field_type=='playa')
+				$field_class = $field_base . '_' . $field_type; // eg Reefine_field_playa
+			else	
+				$field_class = $field_base; // eg Reefine_field
+			// instanciate our object
+			if (class_exists($field_class)) {
+				return new $field_class($this, $field_name, $field_name, '');
+			} else {
+				// class not found
+				throw new Exception('Reefine error: ' . $field_class . ' not supported.');
+			}
 		}
 	}
 }
@@ -1388,7 +1398,7 @@ class Reefine_field {
 	
 	protected $channel_titles_alias = '';
 	
-	function __construct($reefine, $field_name) {
+	function __construct($reefine, $field_name,$parent_field_name) {
 		
 		$this->reefine = $reefine;
 		$this->field_name = $field_name;
@@ -1525,7 +1535,7 @@ class Reefine_field_publisher extends Reefine_field {
 	private $session_language_id;
 	
 	function __construct($reefine,$field_name,$ee_field_name) {
-		parent::__construct($reefine, $ee_field_name);
+		parent::__construct($reefine, $ee_field_name, '');
 		
 		$this->reefine = $reefine;
 		
@@ -1681,6 +1691,89 @@ class Reefine_field_playa extends Reefine_field {
 		return $this->get_field_by_name($this->parent_field_name);
 	}
 }
+
+
+class Reefine_field_publisher_playa extends Reefine_field {
+
+	private $relation_field_id;
+
+	private $child_field_name;
+	private $parent_field_name;
+	private $table_alias;
+	private $table_alias_titles;
+	private $table_alias_data;
+	private $session_language_id;
+	
+	function __construct($reefine,$field_name,$parent_field_name,$child_field_name) {
+		parent::__construct($reefine, $parent_field_name, '');
+		$dbprefix = $reefine->dbprefix;
+		//$this->channel_data_alias = "{$dbprefix}publisher_data";
+		//$this->channel_titles_alias = "{$dbprefix}publisher_titles";
+		$this->reefine = $reefine;
+
+		$this->relation_field_id = $this->get_field_by_key($parent_field_name, 'field_id');
+
+		$this->parent_field_name = $parent_field_name;
+		$this->child_field_name=$child_field_name;
+
+		$this->table_alias = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$this->relation_field_id);
+		$this->table_alias_titles = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$this->relation_field_id) . '_titles';
+		$this->table_alias_data = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$this->relation_field_id) . '_data';
+
+		$this->session_language_id = intval($this->reefine->EE->publisher_model->current_language['id']);
+
+	}
+
+	function get_value_column() {
+		if ($this->child_field_name=='')
+			// Return url_title so we get a nice url for list filters
+			return "{$this->table_alias_titles}.url_title";
+		else if ($this->child_field_name=='title')
+			// return full title, good for search filters
+			return "{$this->table_alias_titles}.title";
+		else
+			// return column data
+			return "{$this->table_alias_data}." . $this->get_field_by_key($this->child_field_name,'field_column');
+	}
+
+	function get_title_column() {
+		if ($this->child_field_name=='' || $this->child_field_name=='title')
+			return "{$this->table_alias_titles}.title";
+		else
+			return "{$this->table_alias_data}." . $this->get_field_by_key($this->child_field_name,'field_column');
+	}
+
+	function get_join_sql() {
+		// more joins than you thought humanly possible
+		$joins = array();
+		// join the publisher table
+		$joins[] = "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_data " .
+		"ON {$this->reefine->dbprefix}publisher_data.entry_id = {$this->channel_data_alias}.entry_id " .
+		"AND {$this->reefine->dbprefix}publisher_data.publisher_status IN ('', " . $this->reefine->db->escape($this->reefine->status) . ") " .
+		"AND {$this->reefine->dbprefix}publisher_data.publisher_lang_id = {$this->session_language_id} ";
+		// join the playa relationship table 
+		$joins[] = "LEFT OUTER JOIN {$this->reefine->dbprefix}playa_relationships {$this->table_alias} " .
+		"ON {$this->table_alias}.parent_entry_id = {$this->reefine->dbprefix}publisher_data.entry_id " .
+		"AND {$this->table_alias}.parent_field_id = {$this->relation_field_id} " .
+		"AND {$this->table_alias}.publisher_lang_id = {$this->session_language_id} " .
+		"AND {$this->table_alias}.publisher_status = 'open' ";
+		// if we just need the titles for "relation" or "relation:title" fields
+		if ($this->child_field_name=='' || $this->child_field_name=='title')
+			$joins[] = "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_titles {$this->table_alias_titles} " .
+			"ON {$this->table_alias_titles}.entry_id = {$this->table_alias}.child_entry_id " .
+			"AND {$this->table_alias_titles}.publisher_lang_id = {$this->session_language_id} ";
+		else
+			$joins[] = "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_data {$this->table_alias_data} " .
+			"ON {$this->table_alias_data}.entry_id = {$this->table_alias}.child_entry_id " .
+			"AND {$this->table_alias_data}.publisher_lang_id = {$this->session_language_id} ";
+		return $joins;
+	}
+
+	function get_field() {
+		return $this->get_field_by_name($this->parent_field_name);
+	}
+}
+
 
 class Reefine_field_grid extends Reefine_field {
 
