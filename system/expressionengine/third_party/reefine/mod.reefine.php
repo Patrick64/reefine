b67924c5c9b1fe96fecdbf166c20a8e299904543
Patrick Woodcock <patrick@ralphmedia.co.uk>
2014-10-31 14:17:53 +0000

    fix for new ver of publisher and added table alias for publisher to fix clashing table names bug

--- system/expressionengine/third_party/reefine/mod.reefine.php
+++ system/expressionengine/third_party/reefine/mod.reefine.php
@@ -1546,6 +1546,9 @@ class Reefine_field_store extends Reefine_field {
 class Reefine_field_publisher extends Reefine_field {
 	
 	private $session_language_id;
+	private $table_alias;
+	private $table_alias_titles;
+	private $table_alias_data;
 	
 	function __construct($reefine,$field_name,$ee_field_name,$child_field_name='') {
 		parent::__construct($reefine, $ee_field_name, '');
@@ -1554,15 +1557,20 @@ class Reefine_field_publisher extends Reefine_field {
 		
 		$this->field_name = $ee_field_name;
 		
-		$this->session_language_id = intval($this->reefine->EE->publisher_model->current_language['id']);
+		$this->session_language_id = intval($this->reefine->EE->publisher_language->current_language['id']);
+		
+		// $this->table_alias = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$ee_field_name);
+		$this->table_alias_titles = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$ee_field_name) . '_titles';
+		$this->table_alias_data = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$ee_field_name) . '_data';
+		
 		
 	}
 	
 	function get_value_column() {
 		if ($this->db_column=='title')
-			return "{$this->reefine->dbprefix}publisher_titles.title";
+			return "{$this->table_alias_titles}.title";
 		else
-			return "{$this->reefine->dbprefix}publisher_data.{$this->db_column}";
+			return "{$this->table_alias_data}.{$this->db_column}";
 	}
 	
 	function get_title_column() {
@@ -1570,15 +1578,15 @@ class Reefine_field_publisher extends Reefine_field {
 	}
 	
 	function get_join_sql() {
-		$joins = array( "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_data " .
-		"ON {$this->reefine->dbprefix}publisher_data.entry_id = {$this->channel_data_alias}.entry_id " .
-		"AND " . $this->reefine->get_status_where_clause($this->reefine->status,"{$this->reefine->dbprefix}publisher_data.publisher_status") .
-		"AND {$this->reefine->dbprefix}publisher_data.publisher_lang_id = {$this->session_language_id} ");
+		$joins = array( "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_data {$this->table_alias_data} " .
+		"ON {$this->table_alias_data}.entry_id = {$this->channel_data_alias}.entry_id " .
+		"AND " . $this->reefine->get_status_where_clause($this->reefine->status,"{$this->table_alias_data}.publisher_status") .
+		"AND {$this->table_alias_data}.publisher_lang_id = {$this->session_language_id} ");
 		if ($this->db_column=='title')
-			$joins[] = "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_titles " .
-			"ON {$this->reefine->dbprefix}publisher_titles.entry_id = {$this->channel_data_alias}.entry_id " .
-			"AND " . $this->reefine->get_status_where_clause($this->reefine->status,"{$this->reefine->dbprefix}publisher_titles.publisher_status") . 
-			"AND {$this->reefine->dbprefix}publisher_titles.publisher_lang_id = {$this->session_language_id} ";
+			$joins[] = "LEFT OUTER JOIN {$this->reefine->dbprefix}publisher_titles {$this->table_alias_titles} " .
+			"ON {$this->table_alias_titles}.entry_id = {$this->channel_data_alias}.entry_id " .
+			"AND " . $this->reefine->get_status_where_clause($this->reefine->status,"{$this->table_alias_titles}.publisher_status") . 
+			"AND {$this->table_alias_titles}.publisher_lang_id = {$this->session_language_id} ";
 		return $joins;
 	}
 	
@@ -1742,7 +1750,8 @@ class Reefine_field_publisher_playa extends Reefine_field {
 		$this->table_alias_titles = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$this->relation_field_id) . '_titles';
 		$this->table_alias_data = 'relation_' . preg_replace('/[^A-Z0-9]/i','_',$this->relation_field_id) . '_data';
 
-		$this->session_language_id = intval($this->reefine->EE->publisher_model->current_language['id']);
+		$this->session_language_id = intval($this->reefine->EE->publisher_language->current_language['id']);
+		
 
 	}
 
