c9e2c3c9e515bf14e29c14557d5bd77cda521edc
Patrick Woodcock <patrick@ralphmedia.co.uk>
2015-02-09 16:46:37 +0000

    added has_active_subfilters

--- system/expressionengine/third_party/reefine/mod.reefine.php
+++ system/expressionengine/third_party/reefine/mod.reefine.php
@@ -1160,7 +1160,7 @@ class Reefine {
 				$tag['active_groups'][] = $group->get_filters_for_output(true);
 			}
 		}
-		
+		//die(json_encode($tag));
 		// parse it
 		return $tag;
 
@@ -3161,6 +3161,17 @@ class Reefine_group_tree extends Reefine_group_list {
 	
 	}
 	
+	
+	/**
+	 * Go though each filter and add the url attribute to filter array
+	 */
+	public function add_filter_url_to_filters() {
+		foreach ($this->filters as &$filter) {			
+			$filter['url'] = $this->reefine->create_url($this->reefine->get_filter_url($this->group_name,$filter['filter_value']));
+		}
+		$this->clear_url = $this->reefine->create_url($this->reefine->get_filter_url($this->group_name,null));
+	}
+	
 
 // 	function compare_filter_for_tree($a, $b){
 // 		if ()
@@ -3233,33 +3244,41 @@ class Reefine_group_tree extends Reefine_group_list {
 	 * @return array
 	 */
 	function get_filters_for_output($only_show_active = false) {
-		$group = array();
-	
-		// get attributes of group
-		foreach (get_object_vars($this) as $key => $val) {
-			if (is_string($val)) {
-				$group[$key] = htmlspecialchars($val, ENT_QUOTES);
+		if ($only_show_active) {
+			return parent::get_filters_for_output(true);
+		} else {
+
+			$group = array();
+
+			// get attributes of group
+			foreach (get_object_vars($this) as $key => $val) {
+				if (is_string($val)) {
+					$group[$key] = htmlspecialchars($val, ENT_QUOTES);
+				}
 			}
+
+			// format filters for output
+			$group['filters'] = array();
+			$group['active_filters'] = $this->active_filters;
+			$group['total_filters'] = $this->total_filters;
+			$group['matching_filters'] = $this->matching_filters;
+			$filter_values = array();
+			foreach ($this->values as $filter_value) {
+				$filter_values[] = array('value'=>$filter_value);
+			}
+			$group['active_filter_values'] = $filter_values;
+
+			// extract $subfilters, $has_active_subfilters
+			extract($this->get_subfilters_for_output($only_show_active,0,0));
+			$group['filters'] = $subfilters;
+
+			return $group;
 		}
-	
-		// format filters for output
-		$group['filters'] = array();
-		$group['active_filters'] = $this->active_filters;
-		$group['total_filters'] = $this->total_filters;
-		$group['matching_filters'] = $this->matching_filters;
-		$filter_values = array();
-		foreach ($this->values as $filter_value) {
-			$filter_values[] = array('value'=>$filter_value);
-		}
-		$group['active_filter_values'] = $filter_values;
-	
-		$group['filters'] = $this->get_subfilters_for_output($only_show_active,0,0);
-		
-		return $group;
 	}
-	
+
 	private function get_subfilters_for_output($only_show_active,$parent_id,$level) {
 		$output_filters = array();
+		$has_active_filters = false;
 		foreach ($this->filters as $filter_key => $filter) {
 			
 			if ($filter['parent_id']==$parent_id) {
@@ -3273,8 +3292,10 @@ class Reefine_group_tree extends Reefine_group_list {
 			
 					$filter_out = array();
 			
-					if ($filter['filter_active'])
+					if ($filter['filter_active']) {
 						$active_index += 1;
+						$has_active_filters = true;
+					}
 					// used for formatting
 					//$filter_out['active_index'] = $active_index;
 					$filter_out['filter_active_class'] = ( $filter_active ? 'active' : 'inactive' );
@@ -3288,6 +3309,14 @@ class Reefine_group_tree extends Reefine_group_list {
 					// number range doessome stuff
 					$this->format_filter_for_output($filter,$filter_out);
 					
+					// $subfilters, $has_active_subfilters 
+					extract($this->get_subfilters_for_output($only_show_active, $filter['filter_id'],$level+1));
+					
+					// set values for if it has an active subfilter, useful for expand/collapse trees
+					$filter_out['has_active_subfilters'] = $has_active_subfilters;
+					$filter_out['has_active_subfilters_class'] = ( $has_active_subfilters ? 'has-active-subfilters' : 'no-active-subfilters' );
+						
+					
 					// if level 2 or more then add _2, _3... to end of value coz of EE's rubbish templateparsing 
 					if ($level>0) {
 						$filter_out2 = array();
@@ -3298,8 +3327,8 @@ class Reefine_group_tree extends Reefine_group_list {
 						unset($val);
 					}
 					
-					$subfilters = $this->get_subfilters_for_output($only_show_active, $filter['filter_id'],$level+1);
 					
+					// get subfilters
 					if (count($subfilters)>0)
 						$filter_out['subfilters_' . ($level+1)] = array(array('filters_' . ($level+1) =>$subfilters));
 					else
@@ -3308,7 +3337,8 @@ class Reefine_group_tree extends Reefine_group_list {
 				}
 			}
 		}
-		return $output_filters;
+		// sned back filters and whether it has an active filter or subfilter
+		return array('subfilters'=>$output_filters,'has_active_subfilters'=>$has_active_filters || $has_active_subfilters);
 			
 	}	
 	
