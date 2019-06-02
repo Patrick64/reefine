6284b6a81fc237d2b120920cc52a796a0c3c9ac4
Patrick <pmwoodcock@gmail.com>
2019-06-02 21:08:53 +0100

    updated version to 3.2.1

--- ee5/system/user/addons/reefine/addon.setup.php
+++ ee5/system/user/addons/reefine/addon.setup.php
@@ -5,7 +5,7 @@ return array(
   'author_url'  => 'http://patrickw.tech',
   'name'        => 'Reefine',
   'description' => 'Reefine: Refine, sort, filter your entries',
-  'version'     => '3.2.0',
+  'version'     => '3.2.1',
   'namespace'   => 'Reefine',
   'docs_url' => 'https://www.ralphmedia.co.uk/docs/reefine/'
 );
