efbecd1eed5d6687cee1b46316ffe60ea0b387f7
Patrick <pmwoodcock@gmail.com>
2020-04-25 01:29:12 +0100

    replaced with ee5

--- ee3/system/user/addons/reefine/language/english/reefine_lang.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$lang = array(
-	
-	"reefine_module_name" 			=> 'Reefine',
-	"reefine_module_description" 	=> 'Reefine from Ralph: Refine, sort, filter your entries'
-	); 
--- ee4/system/user/addons/reefine/groups/Reefine_group_dummy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Reefine_group_dummy extends Reefine_group {
-	public $type = 'dummy';
-	public function __construct($reefine,$group_name) {
-		parent::__construct($reefine,$group_name);
-	}
-}
